bde9379b0f934829d249d78a8c85f9aac67bf821
arbitrary33 <2228930284@qq.com>
2025-02-08 19:26:17 +0800

    test
    修改2

--- hello-world/src/main/java/test.java
+++ hello-world/src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("11111");
+        System.out.println("22222");
     }
 }
